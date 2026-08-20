4b7e9732eca0e96018832f1eaf760cb89c69ee18
Hridesh Thakur <65601436+Hridesh-Thakur@users.noreply.github.com>
2026-08-20 22:44:04 +0530

    delete some unwanted lines

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function speak(text) {
     text_speak.pitch = 1;   
     window.speechSynthesis.speak(text_speak);
 }       
-  
+   
 function wishMe() {
     var day = new Date();
     var hour = day.getHours();  
