0fb286a9cee1ff442fb20ec35ffffb71c7d7274c
Hridesh Thakur <65601436+Hridesh-Thakur@users.noreply.github.com>
2026-08-30 21:48:24 +0530

    30-08-09

--- another_testing.js
+++ another_testing.js
@@ -7,14 +7,14 @@ function speak(text) {
   text_speak.volume = 1; 
   text_speak.pitch = 1;
   window.speechSynthesis.speak(text_speak);
-}     
+}      
   
 function wishMe() { 
   const hour = new Date().getHours();  
   if (hour < 12) speak("Good Morning Boss...How may I help you?");
   else if (hour < 17) speak("Good Afternoon Master...How may I help you?");  
   else speak("Good Evening Sir...How may I help you?");     
-}    
+}       
 
 // ================= COMMAND DATABASE =================
 const commandDB = [
